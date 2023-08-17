4e6b6964c00548c2b3412d689d11809d2d7bc910
Yorick de Wid <yorickdewid@users.noreply.github.com>
2023-08-17 13:17:10 +0200

    Test typecast

--- contrib/testcase.sh
+++ contrib/testcase.sh
@@ -2,6 +2,8 @@
 
 psql -c "SELECT * FROM pg_available_extensions WHERE name = 'iban';"
 
+psql -c "CREATE EXTENSION iban;"
+
 psql -c "CREATE TABLE test_iban (name text, account iban);"
 
 psql -c "INSERT INTO test_iban (name, account) VALUES ('John', 'NL91ABNA0417164300');"
@@ -14,3 +16,5 @@ if [ $? -ne 0 ]; then
     echo "ERROR: Inserting IBAN failed"
     exit 1
 fi
+
+psql -c "SELECT 'KZ86125KZT5004100100'::iban;"
